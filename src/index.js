d5d92ab8bff3b4be01ad72a54ee928662b30be2a
BielovaIryna <belova83@gmail.com>
2023-09-17 23:56:23 +0300

    hw11_1com

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import { fetchImg } from "./js/utils";
+import Notiflix from 'notiflix';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { hitsPerPage, formEl, galleryEl, guardEl, submitEl} from "./js/const";
+
+let page = 1;
+let value = "";
+
+
+const renderGallery = (obj, container) => {
+  const markup = obj.hits.map((e) =>
+    `<div class="photo-card">
+  <img src="${e.webformatURL}" alt="${e.tags}" loading="lazy"  />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${e.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${e.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${e.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${e.downloads}</b>
+    </p>
+  </div>
+</div>`
+  ).join("");
+
+  container.insertAdjacentHTML("beforeend", markup);
+  
+}
+
+const options = {
+  root: null,
+  rootMargin: "100px",
+  threshold: 0,
+};
+const handleIntersection = (entries, observer) => {
+    entries.forEach(async(intersection) => {
+    if  (intersection.isIntersecting) {
+      page += 1;
+      
+      try {
+        const cards = await fetchImg(value, page);
+        let cardsPage=Math.ceil(page*hitsPerPage);
+       
+        if (cardsPage === cards.totalHits) {
+         
+        observer.unobserve(guardEl);
+       return  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+  
+        } else{renderGallery(cards, galleryEl);
+         
+        }
+       
+          } catch (error) {
+        console.log(error);
+      }
+      
+    }
+  });
+  
+}
+const observer = new IntersectionObserver (handleIntersection, options);
+
+
+const onSubmit = async (e) => {
+  e.preventDefault();
+  galleryEl.innerHTML="";
+  const { searchQuery } = formEl.elements;
+  value = searchQuery.value;
+  
+  if (value) {
+    try {
+      const cards = await fetchImg(value, page);
+     console.log(cards);
+      if (cards.hits.length > 1) {
+        Notiflix.Notify.success(`Hooray! We found ${cards.totalHits} images.`);
+       
+        renderGallery(cards, galleryEl);
+        
+      } else {Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")}
+
+        } catch (error) {
+      console.log(error);
+    }
+
+  }
+  
+  
+  observer.observe(guardEl);
+}
+submitEl.addEventListener("click", onSubmit);
+
---
+++ src/js/const.js
@@ -0,0 +1,10 @@
+export const BASE_URL = "https://pixabay.com/api/";
+export const API_KEY = "39430012-65c739e614768df0f6dbfbbc6";
+export const hitsPerPage = 40;
+export const formEl = document.querySelector(".search-form");
+export const submitEl = document.querySelector("button[type=submit]");
+export const galleryEl = document.querySelector(".gallery");
+export const guardEl = document.querySelector(".js-guard");
+
+
+
