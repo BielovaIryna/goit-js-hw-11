b5a8fae2e0d2ea4e73f922ca7847fe7e5413dcf6
BielovaIryna <belova83@gmail.com>
2023-09-19 19:23:08 +0300

    hw11_fixed2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { fetchImg} from "./js/pixabay-api";
+import { fetchImg } from "./js/pixabay-api";
 import { renderGallery } from "./js/render-gallery";
 import Notiflix from 'notiflix';
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { hitsPerPage, formEl, galleryEl, guardEl, submitEl} from "./js/const";
+import { hitsPerPage, formEl, galleryEl, guardEl, submitEl } from "./js/const";
 
 let page = 1;
 let value = "";
@@ -13,65 +13,68 @@ const options = {
   threshold: 0,
 };
 const handleIntersection = (entries, observer) => {
-    
-    entries.forEach(async(intersection) => {
-    if  (intersection.isIntersecting) {
+
+  entries.forEach(async (intersection) => {
+    if (intersection.isIntersecting) {
       page += 1;
-        try {
+      try {
         const cards = await fetchImg(value, page);
-        let cardsPage=Math.ceil(page*hitsPerPage);
-         if (cardsPage >= cards.totalHits) {
-         
-        observer.unobserve(guardEl);
-       return  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-  
-        } else{renderGallery(cards, galleryEl);
-         
+        let cardsPage = Math.ceil(page * hitsPerPage);
+        if (cardsPage >= cards.totalHits) {
+
+          observer.unobserve(guardEl);
+          return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+
+        } else {
+          renderGallery(cards, galleryEl);
+
         }
-       
-          } catch (error) {
+
+      } catch (error) {
         console.log(error);
       }
-      
+
     }
   });
-  
+
 }
-const observer = new IntersectionObserver (handleIntersection, options);
+const observer = new IntersectionObserver(handleIntersection, options);
 
 
 const onSubmit = async (e) => {
-  page=1;
+  page = 1;
   e.preventDefault();
-  galleryEl.innerHTML="";
+  galleryEl.innerHTML = "";
   const { searchQuery } = formEl.elements;
   value = searchQuery.value.trim();
-  
-  if (value==="") {
-    Notiflix.Notify.failure("Input is empty")
-    galleryEl.innerHTML="";
-  } else{
-    try {
-    const cards = await fetchImg(value, page);
-   
-    
-    if (cards.hits.length > 1) {
-      Notiflix.Notify.success(`Hooray! We found ${cards.totalHits} images.`);
-     
-      renderGallery(cards, galleryEl);
-      if(cards.totalHits>hitsPerPage){
-        observer.observe(guardEl);
-      }
-      
-    } else {Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")}
 
-      } catch (error) {
-    console.log(error);
-  }}
- 
-  
-  
-  
+  if (value === "") {
+    Notiflix.Notify.failure("Input is empty")
+    galleryEl.innerHTML = "";
+  } else {
+    try {
+      const cards = await fetchImg(value, page);
+
+
+      if (cards.hits.length > 1) {
+        Notiflix.Notify.success(`Hooray! We found ${cards.totalHits} images.`);
+        console.log(cards);
+        renderGallery(cards, galleryEl);
+        if (cards.totalHits <= hitsPerPage) {
+          return
+        } else {
+          observer.observe(guardEl)
+        };
+      } else { Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.") }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
+
 }
 submitEl.addEventListener("click", onSubmit);
 
